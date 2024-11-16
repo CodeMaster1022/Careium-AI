f14ea274d6cde53a09740f61633c4f6c400f2ca1
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-11-15 23:58:00 -0700

    fix

--- src/redux/features/authSlice.tsx
+++ src/redux/features/authSlice.tsx
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { CreditCard } from 'lucide-react';
 
 // Define the initial state using an interface
 interface User {
