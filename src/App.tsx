afa046d4fd819e9a17d882682dcd73ab2184fbe5
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-11-16 00:39:55 -0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import ChatPage from "./pages/ChatPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/chat" element={<ChatPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <ProtectedRoute path="/chat" element={<ChatPage />} />
         </Routes>
       </BrowserRouter>
     </Provider>
