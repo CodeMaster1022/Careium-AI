1eb9e8042a2fa7744adf909b718cdfba7cfb3bf8
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-11-16 23:17:03 -0700

    login style added

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -88,10 +88,9 @@ export default function LoginPage() {
 
 
   return (
-    <div className="background">
-      <div className="w-full h-screen flex justify-center items-center bg-[#212121]">
-        <div className="background">
-          <div className="w-[400px] rounded-2xl bg-slate-900 py-4">
+      <div className="min-h-screen w-full bg-gradient-to-b from-[#2B4570] to-[#3A5A8C] p-6 flex items-center justify-center">
+        <div className="">
+          <div className="w-full max-w-md p-4 rounded-lg bg-black/10 backdrop-blur-sm md:w-[400px]">
             <div className="flex flex-col gap-5 p-8">
               <p className="text-center text-4xl text-gray-300 mb-4 font-bold">
                 Welcome!
@@ -99,14 +98,14 @@ export default function LoginPage() {
               {method ? (
                 <>
                   <input
-                    className="bg-slate-900 w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-gray-800 text-white"
+                    className="w-full p-3 rounded bg-white/20 border border-white/10 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-white/20"
                     placeholder="username"
                     onChange={(e) => setUsername(e.target.value)}
                     type="text"
                     value={username}
                   />
                   <input
-                    className="bg-slate-900 w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 text-white focus:ring-offset-gray-800"
+                    className="w-full p-3 rounded bg-white/20 border border-white/10 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-white/20"
                     placeholder="password"
                     type="password"
                     onChange={(e) => setPassword(e.target.value)}
@@ -116,7 +115,7 @@ export default function LoginPage() {
               ) : (
                 <>
                   <input
-                    className="bg-slate-900 w-full rounded-lg border border-gray-300 px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 focus:ring-offset-gray-800 text-white"
+                    className="w-full p-3 rounded bg-white/20 border border-white/10 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-white/20"
                     placeholder="Mac address"
                     value={macAddress}
                     onChange={(e) => setMacAddress(e.target.value)}
@@ -126,10 +125,10 @@ export default function LoginPage() {
               <label className="flex cursor-pointer items-center justify-between p-1 text-slate-400">
                 {selectMember ? (
                   <>
-                    <p>Switch reseller</p>
+                    <p className="text-gray-400">Customer Mode</p>
                   </>
                 ) : (
-                  <p>Switch customer</p>
+                  <p className="text-gray-400">Reseller mode</p>
                 )}
                 <div
                   className="relative inline-block"
@@ -141,18 +140,18 @@ export default function LoginPage() {
                     className="peer h-6 w-12 cursor-pointer appearance-none rounded-full border border-gray-300 bg-gary-400 checked:border-green-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2"
                     type="checkbox"
                   />
-                  <span className="pointer-events-none absolute left-1 top-1 block h-4 w-4 rounded-full bg-slate-600 transition-all duration-200 peer-checked:left-7 peer-checked:bg-green-300"></span>
+                  <span className="pointer-events-none absolute left-1 top-1 block h-4 w-4 rounded-full bg-green-300 transition-all duration-200 peer-checked:left-7 peer-checked:bg-green-300"></span>
                 </div>
               </label>
               {selectMember ? (
                 <label className="flex cursor-pointer items-center justify-between p-1 text-slate-400">
-                  {!method ? <p>Use Mac address</p> : <p>Use username</p>}
+                  {!method ? <p className="text-gray-400">Use Mac address</p> : <p className="text-gray-400">Use username</p>}
                   <div className="relative inline-block" onClick={handleRole}>
                     <input
                       className="peer h-6 w-12 cursor-pointer appearance-none rounded-full border border-gray-300 bg-gary-400 checked:border-green-300 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2"
                       type="checkbox"
                     />
-                    <span className="pointer-events-none absolute left-1 top-1 block h-4 w-4 rounded-full bg-slate-600 transition-all duration-200 peer-checked:left-7 peer-checked:bg-green-300"></span>
+                    <span className="pointer-events-none absolute left-1 top-1 block h-4 w-4 rounded-full bg-green-300 transition-all duration-200 peer-checked:left-7 peer-checked:bg-green-300"></span>
                   </div>
                 </label>
               ) : (
@@ -161,7 +160,7 @@ export default function LoginPage() {
               {method ? (
                 <>
                   <button
-                    className="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95"
+                    className="w-full p-3 rounded bg-[#F87777] hover:bg-[#f65e5e] text-white font-medium transition-colors"
                     onClick={handleLoginByRole}
                   >
                     Login By Name
@@ -170,7 +169,7 @@ export default function LoginPage() {
               ) : (
                 <>
                   <button
-                    className="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95"
+                    className="w-full p-3 rounded bg-[#F87777] hover:bg-[#f65e5e] text-white font-medium transition-colors"
                     onClick={handleLoginByMacAddress}
                   >
                     Login By MacAddress
@@ -181,6 +180,5 @@ export default function LoginPage() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
