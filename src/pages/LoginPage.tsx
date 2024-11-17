19a9b58835464ff7b0118712ebf547da31fc56fb
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-11-16 20:22:24 -0700

    updated

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -164,7 +164,7 @@ export default function LoginPage() {
                     className="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95"
                     onClick={handleLoginByRole}
                   >
-                    LoginByName
+                    Login By Name
                   </button>
                 </>
               ) : (
@@ -173,7 +173,7 @@ export default function LoginPage() {
                     className="inline-block cursor-pointer rounded-md bg-gray-700 px-4 py-3.5 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-800 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 active:scale-95"
                     onClick={handleLoginByMacAddress}
                   >
-                    LoginByMacAddress
+                    Login By MacAddress
                   </button>
                 </>
               )}
