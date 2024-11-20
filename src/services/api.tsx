dfbaba97c729d1581270c52e1e4858dee277118c
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-11-19 21:06:13 -0700

    url updated

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://telegram-app-backend-eosin.vercel.app'
+  baseURL: 'https://telegram-app-backend-eosin.vercel.app/api/'
 });
 
 export default api;
