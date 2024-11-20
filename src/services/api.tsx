526d7a322ae8f7fbe7cd625227e8c6aa41445d52
CodeMaster1022 <165914362+CodeMaster1022@users.noreply.github.com>
2024-11-20 12:20:36 -0700

    fix

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'telegram-app-backend-theta.vercel.app/api'
+  baseURL: 'https://telegram-app-backend-theta.vercel.app/api'
 });
 
 export default api;
